84de3684b442c6b62618f287c8c26952e87f1217
tanyaburke <tanyaburke@pursuit.org>
2020-02-06 10:49:29 -0500

    Initial Commit

---
+++ NYTTopStories/Controllers/TopStoriesTabController.swift
@@ -0,0 +1,22 @@
+//
+//  TopStoriesTabController.swift
+//  NYTTopStories
+//
+//  Created by Tanya Burke on 2/6/20.
+//  Copyright © 2020 Tanya Burke. All rights reserved.
+//
+
+import UIKit
+
+class TopStoriesTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .yellow
+    }
+    
+
+    
+
+}
