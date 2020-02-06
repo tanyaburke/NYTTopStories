327811c88c7619bdf34579c3368701c69425ebcf
tanyaburke <tanyaburke@pursuit.org>
2020-02-06 13:03:08 -0500

    Adjusted NetworkHelper

--- NYTTopStories/NetworkServices/TopStoriesAPICLient.swift
+++ NYTTopStories/NetworkServices/TopStoriesAPICLient.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+import NetworkHelper
+
+
+struct NYTTopStoriesAPIClient {
+    static func fetchTopStories(for section: String, completion: @escaping (Result<[Article], AppError>) -> ()) {
+        let endpointURLString = "https://api.nytimes.com/svc/topstories/v2/nyregion.json?api-key="
+        guard let url = URL(string: endpointURLString) else {
+            completion(.failure(.badURL(endpointURLString)))
+            return
+        }
+        let request = URLRequest(url: url)
+        NetworkHelper.shared.performDataTask(with: request) { (result) in
+            switch result {
+            case .failure(let appError):
+                completion(.failure(.networkClientError(appError)))
+            case .success(let data):
+                do {
+                    let topStories = try JSONDecoder().decode(TopStories.self, from: data)
+                    completion(.success(topStories.results))
+                } catch {
+                    //here is where you catch the error
+                    completion(.failure(.decodingError(error)))
+                }
+        }
+    }
+}
+}
+
